32d83ea110d69dc95b919177d2ef2af53c64276e
Michael Treser <armikael2@gmail.com>
2019-09-20 12:24:12 +0300

    Implemented on image hover write console logs.

--- src/app/components/image-list/image-list.component.ts
+++ src/app/components/image-list/image-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import * as M from '../../app.models';
 import { ImageService } from '../../services/image/image.service';
 import { StoreService } from '../../shared/store.service';
+import { fromEvent } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-images-list',
@@ -20,12 +22,27 @@ export class ImageListComponent implements OnInit {
   ngOnInit() {
     this.imageService.getImageList()
       .subscribe((list: M.Image[]) => {
-      console.log(list);
-      this.imageList = list;
-    });
+        this.imageList = list;
+        this.watchImageHover();
+      });
   }
 
-  showImage(selectedImage) {
+  showImage(selectedImage): void {
     this.storeService.setSelectedImage(selectedImage);
+  }
+
+  watchImageHover() {
+    const imageList$ = fromEvent(document, 'mousemove');
+
+    imageList$
+      .pipe(
+        debounceTime(200)
+      )
+      .subscribe((event: MouseEvent) => {
+        const el = event.target as HTMLElement;
+        if (el.className === 'image-list-image') {
+          console.log(el);
+        }
+    });
   }
 }
