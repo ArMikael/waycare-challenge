7799d06ee3246600cae13330ca73c4b5c6186631
Michael Treser <armikael2@gmail.com>
2019-09-20 10:56:26 +0300

    Added virtual scroll for image list.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
 import { ImageListComponent } from './components/image-list/image-list.component';
 import { ImageViewComponent } from './components/image-view/image-view.component';
 
@@ -18,7 +20,8 @@ import { ImageViewComponent } from './components/image-view/image-view.component
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    ScrollingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
