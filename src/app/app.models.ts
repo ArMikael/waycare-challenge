2e12e9e218871ee5ea638096aaed86058d940cbf
Michael Treser <armikael2@gmail.com>
2019-09-20 10:37:44 +0300

    Added pipe to getImageList method to map all image objects with new thumbnail url.

--- src/app/app.models.ts
+++ src/app/app.models.ts
@@ -5,4 +5,5 @@ export interface Image {
   height: number;
   url: string;
   download_url: string;
+  thumbnail?: string;
 }
--- src/app/services/image/image.service.ts
+++ src/app/services/image/image.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { NetService } from '../net/net.service';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import * as M from '../../app.models';
 
 @Injectable({
@@ -9,8 +10,21 @@ import * as M from '../../app.models';
 export class ImageService {
 
   constructor(private netService: NetService) { }
+  thumbnailWidth = 200;
+  thumbnailHeight = 100;
 
   getImageList(): Observable<M.Image[]> {
-    return this.netService.getImageList();
+    return this.netService.getImageList()
+      .pipe(
+        map((imageList: M.Image[]) => {
+          imageList.map((image: any) => {
+            const url = image.download_url.split('/');
+            url.splice(url.length - 2, 2);
+            image.thumbnail = `${url.join('/')}/${this.thumbnailWidth}/${this.thumbnailHeight}`;
+            return image;
+          });
+          return imageList;
+        })
+      );
   }
 }
